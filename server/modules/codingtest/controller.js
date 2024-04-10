5b62e08a6877785f2bfa9cb2a8f2d7bb669e2767
swarup4 <swarup.saha004@hotmail.com>
2024-04-10 16:46:15 +0530

    Complete Submit API functionality

--- server/modules/codingtest/controller.js
+++ server/modules/codingtest/controller.js
@@ -13,36 +13,61 @@ const userMiddleware = require("../../middleware/user");
 
 const router = express.Router();
 
-async function checkApiExist(req, res, next) {
-  try {
+function checkApiExist(req, res, next) {
     const endPoint = req.body.apiEndPoint;
     const version = req.body.version;
-    // { $or: [ { apiEndPoint: endPoint }, { version: version } ] }
-    const api = await UserApiModels.UserAPIs.find({
-      $or: [
-        {
-          $and: [{ apiEndPoint: { $ne: true } }, { condition2: { $ne: true } }],
-        },
-        { condition1: { $ne: true } }, // At least one condition should not be true
-        { condition2: { $ne: true } }, // At least one condition should not be true
-      ],
+
+    Models.APIs.find({
+        apiEndPoint: endPoint, version: version
+    }).then(api => {
+        if (api.length > 0) {
+            console.log("More than 1 ===>>> ", api);
+            res.json({
+                status: 200,
+                data: "Someone already Submit the API"
+            })
+        } else {
+            console.log("Empty Data ===>>> ", api);
+            next()
+        }
+    }).catch(err => {
+        console.log("Error ===>>> ", err);
+        res.send(err);
     });
-    if (api) {
-    }
-  } catch (error) {}
+}
+
+function getApiInfo(req, res, next) {
+    const endPoint = req.body.apiEndPoint;
+    const version = req.body.version;
+    const userId = req.body.userId;
+
+    UserApiModels.UserAPIs.findOne({
+        apiEndPoint: endPoint, version: version, userId: userId
+    }).then(api => {
+        req.apiData = api;
+        next()
+    }).catch(err => {
+        res.send(err);
+    });
 }
 
 // API
-router.post("/submitApi", async (req, res) => {
-  try {
-    const model = new Models.APIs(req.body);
-    const api = await model.save();
-    if (api) {
-      res.json(api);
+router.post("/submitApi", checkApiExist, getApiInfo, async (req, res) => {
+    try {
+        let obj = {
+            submitedBy: req.apiData.userId,
+            apiId: req.apiData._id,
+            version: req.apiData.version,
+            apiEndPoint: req.apiData.apiEndPoint,
+        }
+        const model = new Models.APIs(obj);
+        const api = await model.save();
+        if (api) {
+            res.json(api);
+        }
+    } catch (error) {
+        res.send(error);
     }
-  } catch (error) {
-    res.send(error);
-  }
 });
 
 module.exports = router;
