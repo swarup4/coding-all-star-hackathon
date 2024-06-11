690a92d1491b060a22288525a646d3acc54e0cdc
Swarup Saha <swarup.saha004@hotmail.com>
2024-06-11 12:55:11 +0530

    Fix UI Issue

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -86,6 +86,14 @@ function App() {
                 }, {
                     path: 'prize',
                     element: <AddPrize />
+                }, {
+                    path: 'profile',
+                    element: <Profile />,
+                    errorElement: <Error />
+                }, {
+                    path: 'profile/edit',
+                    element: <ProfileInfo />,
+                    errorElement: <Error />
                 }]
             }],
         }, {
