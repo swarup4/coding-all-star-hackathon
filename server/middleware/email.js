b54346fb555499c7828785262c4251011f3b3794
Swarup Saha <swarup.saha004@hotmail.com>
2024-06-12 13:02:16 +0530

    Change Email Template

--- server/middleware/email.js
+++ server/middleware/email.js
@@ -15,10 +15,11 @@ const SES_Config = {
 const sesClient = new SESClient(SES_Config);
 
 module.exports = (user, hackathon) => {
-    console.log(user.email);
+    let firstName = user.name.split(' ')[0]
+
     let params = {
         Destination: {
-            CcAddresses: ['strangefriendship3@gmail.com'],
+            CcAddresses: [],
             ToAddresses: [user.email],
         },
         Source: 'swarup_s@trigent.com',
@@ -28,25 +29,24 @@ module.exports = (user, hackathon) => {
                 Html: {
                     Charset: 'UTF-8',
                     Data: `
-                    <b>Hello ${user.name},</b>
+                    <p>Hello ${firstName},</p>
                     <p>New Hackathon has coming. Please check "${hackathon}" hackathon</p>
                     <p>Please find the below your credentials for All Star Hackathon platform. All the best !!</p>
 
-                    <p><b>URL: </b><a href='http://13.200.85.53:3000'><span>https://hackathon.trigent.com</span></a></p>
+                    <p>
+                        <div>URL: <a href='http://13.200.85.53:3000'><span>https://hackathon.trigent.com</span></a></div>
+                        <div>Username: <span>${user.email}</span></div>
+                        <div>Password: <span>${user.password}</span></div>
+                    </p>
 
-                    <p><b>Username: </b><span>${user.email}</span></p>
-
-                    <p><b>Password: </b><span>${user.password}</span></p>
-
-
-                    <p>Regards,</p>
-                    <p>System</p>
+                    <div>Regards,</div>
+                    <div>System</div>
                     `
                 },
                 Text: {
                     Charset: 'UTF-8',
                     Data: `
-                    Hello ${user.name},
+                    Hello ${firstName},
                     New Hackathon has coming. Please check "${hackathon}" hackathon
                     Please find the below your credentials for All Star Hackathon platform. All the best !!
 
