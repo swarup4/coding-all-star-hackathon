24f04fae6a81626e264af245baf7dff353b1583c
Swarup Saha <swarup.saha004@hotmail.com>
2024-07-19 12:36:51 +0530

    Update Login Placeholder

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -105,7 +105,7 @@ export default function Login() {
 
                             <div className="mb-6">
                                 <label className="block mb-2 text-coolGray-800 font-medium" htmlFor="">Email <span className='text-red-500'>*</span></label>
-                                <input type="email" placeholder="dev@shuffle.dev" name="email" autoComplete="email" value={values.email} onChange={handleChange} onBlur={handleBlur}
+                                <input type="email" placeholder="dev@trigent.com" name="email" autoComplete="email" value={values.email} onChange={handleChange} onBlur={handleBlur}
                                     className="appearance-none block w-full p-3 leading-5 text-coolGray-900 border border-coolGray-200 rounded-lg shadow-md placeholder-coolGray-400 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50" />
                                 {errors.email && touched.email ? (
                                     <p className='mt-1 text-red-500'>{errors.email}</p>
