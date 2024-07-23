264109df2ff76a8d286e346d095e158bbc9ebc80
Swarup Saha <swarup.saha004@hotmail.com>
2024-07-23 22:10:32 +0530

    Change Host URL

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,2 +1,2 @@
-// export const HOST_URL = 'https://allstar.trigent.com:3001/'
-export const HOST_URL = 'http://localhost:3001/'
+export const HOST_URL = 'https://allstar.trigent.com:3001/'
+// export const HOST_URL = 'http://localhost:3001/'
