36dde72de5747fe7118697d28780c7701f982838
Swarup Saha <swarup.saha004@hotmail.com>
2024-07-29 16:07:15 +0530

    Change Icon

--- app/src/components/common/PopoverDialog.jsx
+++ app/src/components/common/PopoverDialog.jsx
@@ -1,5 +1,5 @@
 import { Popover, Transition } from '@headlessui/react'
-import { InformationCircleIcon } from '@heroicons/react/20/solid'
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
 import { Fragment } from 'react'
 
 export default function PopoverDialog(props) {
